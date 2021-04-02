349f7b27d78a605477cd4feac54b1023cdc45632
maenmulhem <maen@gta-canada.com>
2021-04-01 19:01:43 -0600

    inti

---
+++ ConsoleApp2/Models/Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2.Models
+{
+    class Square : Shape
+    {
+        #region Constructors
+        
+        public Square():base()
+        {
+            
+            
+        }
+
+        public override void AbstractFunction()
+        {
+            Console.WriteLine("Overrided in Square");
+        }
+
+        #endregion
+
+        #region Functions
+        public float area()
+        {
+            f();
+            Console.WriteLine(inheritedProtected());
+            return 1.5f;
+        }
+        private float f()
+        {
+            return 1;
+        }
+        #endregion
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ConsoleApp2.Models;
+using System.Collections.Generic;
+namespace ConsoleApp2
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> uniqueNoList = new List<int>();
+            uniqueNoList = UniqueRandomNoList(10000, 10000);
+            foreach (int no in uniqueNoList)
+            {
+                Console.WriteLine(no);
+            }
+            Console.ReadLine();
+        }
+        public static List<int> UniqueRandomNoList(int maxRange, int totalRandomnoCount)
+        {
+
+            List<int> noList = new List<int>();
+            int count = 0;
+            Random r = new Random();
+            List<int> listRange = new List<int>();
+            for (int i = 0; i < totalRandomnoCount; i++)
+            {
+                listRange.Add(i);
+            }
+            while (listRange.Count > 0)
+            {
+                int item = r.Next(maxRange);// listRange[];    
+                if (!noList.Contains(item) && listRange.Count > 0)
+                {
+                    noList.Add(item);
+                    listRange.Remove(count);
+                    count++;
+                }
+            }
+            return noList;
+        }
+    }
+}
