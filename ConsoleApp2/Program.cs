735ef006796fb032161e2396e202afa22be1acfe
maenmulhem <maen@gta-canada.com>
2021-04-01 20:12:26 -0600

    Add some comments

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,20 +9,25 @@ namespace ConsoleApp2
 
         static void Main(string[] args)
         {
-            List<int> uniqueNoList = new List<int>();
-            uniqueNoList = UniqueRandomNoList(10, 10);
-            foreach (int no in uniqueNoList)
+            // Let's generate the numbers
+            List<int> uniquechosenNumbers = UniqueRandomchosenNumbers(10000, 10000);
+            
+            // Then let's print them
+            foreach (int no in uniquechosenNumbers)
             {
                 Console.WriteLine(no);
             }
             Console.ReadLine();
         }
-        public static List<int> UniqueRandomNoList(int maxRange, int totalRandomnoCount)
+        public static List<int> UniqueRandomchosenNumbers(int maxRange, int totalRandomnoCount)
         {
-
-            List<int> noList = new List<int>();
-            int count = 0;
+            // List holds the previous chosen numbers
+            List<int> chosenNumbers = new List<int>();
+            
+            // Random numbers generator
             Random r = new Random();
+
+            // Range of numbers 
             List<int> listRange = new List<int>();
             for (int i = 1; i <= totalRandomnoCount; i++)
             {
@@ -30,16 +35,17 @@ namespace ConsoleApp2
             }
             while (listRange.Count > 0)
             {
-                int item = r.Next(1,maxRange+1);// listRange[];
-                //Console.WriteLine(listRange[0]);
-                if (!noList.Contains(item) && listRange.Count > 0)
+                int item = r.Next(1,maxRange+1);// a random number in range [1,10000] 
+                // if the random number has not been chosen yet and the we still can chose 
+                if (!chosenNumbers.Contains(item) && listRange.Count > 0)
                 {
-                    noList.Add(item);
+                    // let's add the random number to the list of the previous chosen numbers 
+                    chosenNumbers.Add(item);
+                    // and remove it from
                     listRange.Remove(item);
-                    count++;
                 }
             }
-            return noList;
+            return chosenNumbers;
         }
     }
 }
