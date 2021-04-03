2a64f1db60af827e7ddf7bcda7a26c49656dd949
maenmulhem <maen@gta-canada.com>
2021-04-03 11:00:41 -0600

    Solve based on a counter

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using ConsoleApp2.Models;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -29,22 +28,17 @@ namespace ConsoleApp2
             // Random numbers generator
             Random r = new Random();
 
-            // Range of numbers 
-            List<int> listRange = new List<int>();
-            for (int i = 1; i <= totalRandomnoCount; i++)
-            {
-                listRange.Add(i);
-            }
-            while (listRange.Count > 0)
+            int counter = 0;
+          
+            while (counter < totalRandomnoCount)
             {
                 int item = r.Next(1,maxRange+1);// a random number in range [1,10000] 
-                // if the random number has not been chosen yet O(1) and the we still can chose 
-                if (!chosenNumbers.ContainsKey(item) && listRange.Count > 0)
+                // if the random number has not been chosen yet O(1) and if we still can chose 
+                if (!chosenNumbers.ContainsKey(item))
                 {
                     // let's add the random number to the list of the previous chosen numbers 
                     chosenNumbers.Add(item,item);
-                    // and remove it from
-                    listRange.Remove(item);
+                    counter++;
                 }
             }
             return chosenNumbers.Keys.ToList();
