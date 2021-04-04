2b987d1e4c80ecf09d8d8377b8b43587f2d9b8f8
maenmulhem <maen@gta-canada.com>
2021-04-04 14:38:56 -0600

    Solution 2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,9 +10,14 @@ namespace ConsoleApp2
 
         static void Main(string[] args)
         {
+            const int n = 10000;
             // Let's generate the numbers
-            List<int> uniquechosenNumbers = UniqueRandomchosenNumbers(10000, 10000);
-            
+            // Solution 1 : consumes linear memory n = 10000 and check if condition more than solution 2    
+            //List<int> uniquechosenNumbers = solution1(n);
+
+            // Soltion 2 : consumes linear memory n = 10000 and less if condition checking operations
+            List<int> uniquechosenNumbers = solution2(n);
+
             // Then let's print them
             foreach (int no in uniquechosenNumbers)
             {
@@ -20,28 +25,56 @@ namespace ConsoleApp2
             }
             Console.ReadLine();
         }
-        public static List<int> UniqueRandomchosenNumbers(int maxRange, int totalRandomnoCount)
+        public static List<int> solution1(int maxRange)
         {
             // List holds the previous chosen numbers in a dictionary so we can retrieve the calues in O(1)
-            Dictionary<int, int> chosenNumbers = new Dictionary<int, int>();
+            Dictionary<int, int> result = new Dictionary<int, int>();
             
             // Random numbers generator
             Random r = new Random();
 
             int counter = 0;
           
-            while (counter < totalRandomnoCount)
+            while (counter < maxRange)
             {
                 int item = r.Next(1,maxRange+1);// a random number in range [1,10000] 
                 // if the random number has not been chosen yet O(1) and if we still can chose 
-                if (!chosenNumbers.ContainsKey(item))
+                if (!result.ContainsKey(item))
                 {
                     // let's add the random number to the list of the previous chosen numbers 
-                    chosenNumbers.Add(item,item);
+                    result.Add(item,item);
                     counter++;
                 }
             }
-            return chosenNumbers.Keys.ToList();
+            return result.Keys.ToList();
         }
+        public static List<int> solution2(int maxRange)
+        {
+            // List holds the previous chosen numbers in a dictionary so we can retrieve the calues in O(1)
+            List<int> result = new List<int>();
+
+            // Random numbers generator
+            Random r = new Random();
+
+            List<int> range = new List<int>();
+
+            // Lets create a list of the required numbers [1,2,3,4 ...etc]
+            for (int i = 1; i <= maxRange; i++)
+            {
+                range.Add(i);
+            }
+            // While the numbmer of elements of the result list is less than the range    
+            while (result.Count < maxRange)
+            {
+                // Chose random index
+                int index = r.Next(0, range.Count);
+                // Add the range[index] into the result
+                result.Add(range[index]);
+                // Remove range[index]
+                range.RemoveAt(index);
+            }
+            return result;
+        }
+
     }
 }
